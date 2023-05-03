05b8814889f3594d077994433365314b69dc72a1
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2023-05-03 15:00:19 -0500

    Inicializacion variables y libreria

---
+++ Prueba.c
@@ -0,0 +1,7 @@
+//Construya un programa que lea por pantalla un número entero y diga el número de dígitos que tiene y la suma de estos.
+#include <stdio.h>
+int main()
+{
+int n,s,d;//n es el numero que debe ser ingresado por el usuario,s es la suma de dichos digitos y d es los digitos
+
+}
