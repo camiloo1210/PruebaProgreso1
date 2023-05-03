346e486f025a5e09e8d88862fa72db2216ab8fcc
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2023-05-03 15:17:38 -0500

    Se añaden sentencias condicionales y procesos necesarioss para obtener digitos y la suma de dichos numeros

--- Prueba.c
+++ Prueba.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 int main()
 {
-int n,s,d;//n es el numero que debe ser ingresado por el usuario,s es la suma de dichos digitos y d es los digitos
-
+    int n,s,d,c=0;//n es el numero que debe ser ingresado por el usuario,s es la suma de dichos digitos y d es los digitos,c es igual al contador de repeticiones 
+    printf("Ingrese un numero entero ");
+    scanf("%i",&n);
+    while (n>0)
+    {
+        d=n%10;//nos permite descomponer el numero de digitos
+        if (d>0)
+        {
+        s=s+d;//Se almacena la suma de dichos digitos
+        }
+        c=c+1;//nos permite saber cuantos digitos hay
+    }
 }
+
+
