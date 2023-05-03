6ba0a7bac664195937d9ba14e5014bc6f282249c
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2023-05-03 15:25:28 -0500

    Impresion de las respuestas requeridas

--- Prueba.c
+++ Prueba.c
@@ -3,7 +3,7 @@
 int main()
 {
     int n,s,d,c=0;//n es el numero que debe ser ingresado por el usuario,s es la suma de dichos digitos y d es los digitos,c es igual al contador de repeticiones 
-    printf("Ingrese un numero entero ");
+    printf("Ingrese un numero entero \n");
     scanf("%i",&n);
     while (n>0)
     {
@@ -15,6 +15,8 @@ int main()
         c=c+1;//nos permite saber cuantos digitos hay
         n=n/10;//Se divide para 10 al mismo numero para ir "Desapareciendo dichos digitos"
     }
+    printf("La suma de todos los digitos es= %i\n",s);//Se muestra la suma de todos los digitos ingresados
+    printf("La cantidad de digitos que hay en el numero ingresado es= %i\n",c);//Se muestra la cantidad de digitos que se ingrrso 
 }
 
 
